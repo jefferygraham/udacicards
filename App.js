42edd946c31eafe091142538dedcdd1361916d5c
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-10-23 12:27:06 -0400

    chore: create store

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
-
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import Main from './components/Main';
+import reducer from './reducers';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    <Provider store={createStore(reducer, applyMiddleware(thunk, logger))}>
+      <NavigationContainer>
+        <Main />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
--- utils/api.js
+++ utils/api.js
@@ -4,3 +4,31 @@
 // addCardToDeck(title, card): take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
 
 // Notice each deck creates a new key on the object. Each deck has a title and a questions key. title is the title for the specific deck and questions is an array of questions and answers for that deck.
+
+export function decks() {
+  return {
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces',
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event',
+        },
+      ],
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer:
+            'The combination of a function and the lexical environment within which that function was declared.',
+        },
+      ],
+    },
+  };
+}
