459ae8e260c68169db7b12d9729f6b3727b11cc6
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-10-24 19:46:22 -0400

    feature: add info for quiz screen

--- components/DeckList.js
+++ components/DeckList.js
@@ -8,21 +8,6 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-// const DATA = [
-//   {
-//     id: '1',
-//     title: 'First Item',
-//   },
-//   {
-//     id: '2',
-//     title: 'Second Item',
-//   },
-//   {
-//     id: '3',
-//     title: 'Third Item',
-//   },
-// ];
-
 class DeckList extends Component {
   render() {
     const { navigate } = this.props.navigation;
--- components/Quiz.js
+++ components/Quiz.js
@@ -7,27 +7,66 @@ import {
   Text,
   StyleSheet,
 } from 'react-native';
+import { connect } from 'react-redux';
 
-function Quiz() {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        marginTop: StatusBar.currentHeight || 0,
-        alignItems: 'center',
-      }}
-    >
-      <View style={{ alignSelf: 'flex-start' }}>
-        <Text>1/5</Text>
-      </View>
-      {true ? (
+class Quiz extends Component {
+  state = {
+    showAnswer: false,
+    questionIdx: 0,
+  };
+
+  nextQuestion = (decks, deckTitle) => {
+    let numQuestions = decks[deckTitle].questions.length;
+    if (this.state.questionIdx < numQuestions - 1) {
+      this.setState(() => ({
+        questionIdx: this.state.questionIdx + 1,
+      }));
+    }
+  };
+
+  render() {
+    const { navigation, route, decks } = this.props;
+    const { deckTitle } = route.params;
+
+    const deck = decks[deckTitle];
+
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          marginTop: StatusBar.currentHeight || 0,
+          alignItems: 'center',
+        }}
+      >
+        <View style={{ alignSelf: 'flex-start' }}>
+          <Text>
+            {this.state.questionIdx + 1}/{deck.questions.length}
+          </Text>
+        </View>
         <View style={{ flex: 1, justifyContent: 'space-around' }}>
-          <View>
-            <Text style={{ textAlign: 'center' }}>QUESTION TEXT</Text>
-            <TouchableOpacity>
-              <Text style={{ textAlign: 'center' }}>Answer</Text>
-            </TouchableOpacity>
-          </View>
+          {this.state.showAnswer ? (
+            <View>
+              <Text style={{ textAlign: 'center' }}>
+                {deck.questions[this.state.questionIdx].answer}
+              </Text>
+              <TouchableOpacity
+                onPress={() => this.setState({ showAnswer: false })}
+              >
+                <Text style={{ textAlign: 'center' }}>Question</Text>
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <View>
+              <Text style={{ textAlign: 'center' }}>
+                {deck.questions[this.state.questionIdx].question}
+              </Text>
+              <TouchableOpacity
+                onPress={() => this.setState({ showAnswer: true })}
+              >
+                <Text style={{ textAlign: 'center' }}>Answer</Text>
+              </TouchableOpacity>
+            </View>
+          )}
           <View>
             <TouchableOpacity style={styles.button}>
               <Text style={{ textAlign: 'center' }}>Correct</Text>
@@ -36,27 +75,18 @@ function Quiz() {
               <Text style={{ textAlign: 'center' }}>Incorrect</Text>
             </TouchableOpacity>
           </View>
-        </View>
-      ) : (
-        <View style={{ flex: 1, justifyContent: 'space-around' }}>
           <View>
-            <Text style={{ textAlign: 'center' }}>YES/NO</Text>
-            <TouchableOpacity>
-              <Text style={{ textAlign: 'center' }}>Question</Text>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <TouchableOpacity style={styles.button}>
-              <Text style={{ textAlign: 'center' }}>Correct</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <Text style={{ textAlign: 'center' }}>Incorrect</Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.nextQuestion(decks, deckTitle)}
+            >
+              <Text style={{ textAlign: 'center' }}>Next</Text>
             </TouchableOpacity>
           </View>
         </View>
-      )}
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -67,4 +97,10 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Quiz;
+function mapStateToProps(state) {
+  return {
+    decks: state,
+  };
+}
+
+export default connect(mapStateToProps)(Quiz);
